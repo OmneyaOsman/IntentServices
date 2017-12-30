17535ba569910ea4dffee3cac1b57d96255e8b02
Omni <engomneya.osman@gmail.com>
2017-12-30 18:45:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -0,0 +1,41 @@
+package com.example.android.background.sync;/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+
+//  TODO (10) Create a default constructor that calls super with the name of this class
+
+//  TODO (11) Override onHandleIntent
+
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class WaterReminderIntentService extends IntentService {
+
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //      TODO (12) Get the action from the Intent that started this Service
+//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        String action = intent.getAction();
+         ReminderTasks.executeTask(getApplicationContext() ,action);
+    }
+}
